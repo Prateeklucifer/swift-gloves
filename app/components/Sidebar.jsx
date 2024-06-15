bfe78bc0cd4fff6532d16b99460ff8f085676800
Prateek Yadu <prateekyadudotdev@gmail.com>
2024-06-15 10:24:30 +0530

    refactor - code refactor

--- app/components/Sidebar.jsx
+++ app/components/Sidebar.jsx
@@ -37,7 +37,7 @@ export default function Sidebar() {
     <div className="text-white py-4 px-4 flex items-center gap-4 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6 md:h-screen md:flex-col bg-gray-800 md:items-start md:fixed top-0 left-0">
       <button className="forMobi md:hidden" onClick={() => { handleHamburger() }}><HiOutlineBars3 size={30} /></button>
 
-      <Link href={"/admin"} className="logo font-semibold text-xl">Pixelated<span className="text-sky-400">Professor</span></Link>
+      <Link href={"/admin"} className="logo font-semibold text-xl">Pixelated<span className="text-[#00FFFF]">Professor</span></Link>
       <div className={`${isOpen ? "flex" : "hidden"} overlay absolute top-0 left-0 h-screen w-screen bg-black/40 z-10`}></div>
       <div className={`${isOpen ? "flex z-20 left-0" : "hidden left-32 "} flex-col sidebar absolute top-0  h-screen bg-white w-60 text-neutral-900 py-3 px-4 `}>
         <div className="header flex justify-start items-center">
@@ -54,7 +54,7 @@ export default function Sidebar() {
       <ul className="hidden md:flex flex-col gap-2 my-4 w-full">
         {links.map((link, key) => (
           <li key={key} className="">
-            <Link href={link.href} className={` text-sm py-3 px-4 hover:bg-gray-700 block rounded-lg font-bold ${path == link.href ? "text-sky-400 bg-gray-700 " : " text-sky-50"}`} >{link.name}</Link>
+            <Link href={link.href} className={` text-sm py-3 px-4 hover:bg-gray-700 block rounded-lg font-bold ${path == link.href ? "text-[#00FFFF] bg-gray-700 " : " text-sky-50"}`} >{link.name}</Link>
           </li>
         ))}
       </ul>
